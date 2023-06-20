b86b71fe3b2eaef6e773e5a4ea4e89f5bebe6f0f
lpdink <lpdink@qq.com>
2023-06-20 02:23:21 +0000

    update

--- src/fdl/main.py
+++ src/fdl/main.py
@@ -3,20 +3,59 @@ import argparse
 import types
 import json
 import pkg_resources
+import re
 
 
-def check_valid(json_path):
+def check(json_path):
     """
     1. 必须存在task, hparams, objects三个顶层属性
     2. 必须存在task.name, hparams.save_path
     3. objects中的对象，必须存在name, clazz。对象列表的name必须唯一。
-    4. objects中对象的args引用的对象名必须出现在objects中，且不能循环构造.
+    4. objects中的对象是反序构造的，引用的项目必须首先被构造
     """
+    with open(json_path, "r") as json_file:
+        content = json.load(json_file)
+    assert all(item in content.keys() for item in ["task", "hparams", "objects"]), "json must have attrs:'task', 'hparams', 'objects' at top level."
+    assert "name" in content["task"].keys() and "save_path" in content["hparams"].keys(), "json must have task.name and hparams.save_path"
+    assert isinstance(content["objects"], list), f"objects must be list, got {type(content['objects'])}"
+    obj_names = []
+    for item in content["objects"]:
+        assert isinstance(item, dict), f"item in objects must be dict, got {type(item)}"
+        assert "name" in item.keys() and "clazz" in item.keys(), "item in objects must have attr: 'name', 'clazz'."
+        assert item["name"] not in obj_names, f"item name in objects repeat:{item['name']}"
+        obj_names.append(item["name"])
+    # 按照列表的反序构造，检查被引用项必须首先被构造
+    for item in content["objects"]:
+        name = item["name"]
+        name_idx = obj_names.index(name)
+        if "args" in item.keys():
+            for key, value in item["args"].item():
+                match =re.search(r"\${(.+?)}", str(value)) 
+                if match:
+                    match_str = match.group(1)
+                    assert match_str in obj_names, f"reference '{match_str}' not found in object '{name}'"
+                    reference_idx = obj_names.index(match_str)
+                    assert reference_idx<name_idx, f"objects are constructed  in reverse order, and item '{match_str}' must be constructed before being '{name}' referenced"
+    else:
+        print(f"json {json_file} check pass.")
+
+"""
+fdl -s
+loop: classification, regression, GAN, CGAN, 
+model: aaa, bbb, ccc, ddd
+dataset: aaa, bbb, ccc, ddd, eee
+loss:
+opt:
+
+"""
 
 def buildjson(module_names):
     base_json_path = pkg_resources.resource_filename("fdl", "jsons/base.json")
     modules = ["loop", "model", "dataset"]
-    file_paths = [pkg_resources.resource_filename("fdl", f"jsons/{module}.json") for module in modules]
+    file_paths = [
+        pkg_resources.resource_filename("fdl", f"jsons/{module}.json")
+        for module in modules
+    ]
     usable_modules = dict()
     for file_path in file_paths:
         with open(file_path, "r") as json_file:
@@ -57,7 +96,9 @@ def deploy(json_path):
 
 
 def make_parser():
-    parser = argparse.ArgumentParser(description="fdl:Fast Deep Learning, train/infer/deploy your deep learning model with only a json file. All In One File!")
+    parser = argparse.ArgumentParser(
+        description="fdl:Fast Deep Learning, train/infer/deploy your deep learning model with only a json file. All In One File!"
+    )
     parser.add_argument(
         "-v",
         "--version",
@@ -85,6 +126,9 @@ def make_parser():
         choices=["model", "dataset", "loop", "task", "all"],
         default=None,
         help="show usable modules info.",
+    )
+    parser.add_argument(
+        "-c", "--check", default=None, help="check input json file valid."
     )
     parser.add_argument(
         "-t", "--train", default=None, help="train with input json file."
